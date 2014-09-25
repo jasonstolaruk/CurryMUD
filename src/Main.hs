00c3ab4414bd29d55246807b3ba58166eb6dbcfb
Jason Stolaruk <jason@detroitlabs.com>
2014-09-25 15:52:06 -0400

    石の上にも三年。:station:

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,27 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 {-
-  CurryMud - A Multi-User Dungeon written in Haskell.
-  By Jason Stolaruk.
-  jasonstolaruk (`at` gmail) . com
-  @JasonStolaruk
-  https://github.com/jasonstolaruk/CurryMUD
-  kickButt <$> take maxBound names
+Copyright 2014 Jason Stolaruk and Detroit Labs LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-}
+
+{-
+CurryMud - A Multi-User Dungeon by Jason Stolaruk.
+jasonstolaruk (`at` gmail) . com
+@JasonStolaruk
+https://github.com/jasonstolaruk/CurryMUD
+kickButt <$> take maxBound names
 -}
 
 module Main (main) where
