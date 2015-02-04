2fc04a213cd4a0ede7648c9594530a8b80822d35
Jason Stolaruk <jason@detroitlabs.com>
2015-02-03 23:03:19 -0500

    求刑懲役。:cake:

--- lib/Mud/Cmds/ActionCmds.hs
+++ lib/Mud/Cmds/ActionCmds.hs
@@ -10,17 +10,20 @@ import Mud.Data.State.ActionParams.ActionParams
 import Mud.Data.State.State
 import Mud.Data.State.Util.Output
 import Mud.Data.State.Util.STM
-import Mud.Util.Misc
+import Mud.Util.Misc hiding (patternMatchFail)
+import Mud.Util.Quoting
 import qualified Mud.Logging as L (logPlaOut)
+import qualified Mud.Util.Misc as U (patternMatchFail)
 
-import Data.List (delete)
-import Data.Monoid ((<>))
+import Control.Lens.Getter (view)
+import Control.Lens.Operators ((^.))
+import Data.IntMap.Lazy ((!))
+import Data.List ((\\), delete)
+import Data.Monoid ((<>), mempty)
 import qualified Data.Set as S (Set, fromList, foldr)
 import qualified Data.Text as T
 
 {-
-import Control.Lens.Getter (view)
-import Control.Lens.Operators ((^.))
 import Mud.Cmds.Util.Abbrev
 import Mud.Cmds.Util.Misc
 import Mud.Data.State.ActionParams.Util
@@ -31,16 +34,12 @@ import Mud.NameResolution
 import Mud.TopLvlDefs.Chars
 import Mud.TopLvlDefs.Misc
 import Mud.Util.Padding
-import Mud.Util.Quoting
 import Mud.Util.Wrapping
-import qualified Mud.Util.Misc as U (patternMatchFail)
 -}
 
 
-{-
 patternMatchFail :: T.Text -> [T.Text] -> a
 patternMatchFail = U.patternMatchFail "Mud.Cmds.ActionCmds"
--}
 
 
 -----
@@ -85,5 +84,42 @@ actionCmdBlink (NoArgs'' i) = readWSTMVar >>= \ws ->
         toSelfBrdcst    = (nlnl toSelfMsg, [i])
         toOthersMsg     = serialize d <> " blinks."
         toOthersBrdcst  = (nlnl toOthersMsg, i `delete` pcIds d)
-    in logPlaOut "actionCmdBlink" i [toSelfMsg] >> bcast (toSelfBrdcst : [toOthersBrdcst])
+    in logPlaOut (bracketQuote "action command") i [toSelfMsg] >> bcast (toSelfBrdcst : [toOthersBrdcst])
+actionCmdBlink (OneArg i mq cols target) = readWSTMVar >>= \ws ->
+    let (d, _, _, ri, ris@((i `delete`) -> ris')) = mkCapStdDesig i ws
+        c                                         = (ws^.coinsTbl) ! ri
+    in if (not . null $ ris') || (c /= mempty)
+      then case resolveRmInvCoins i ws [target] ris' c of
+        (_,                    [ Left  [sorryMsg] ]) -> wrapSend mq cols sorryMsg
+        (_,                    Right _:_           ) -> wrapSend mq cols "Sorry, but action commands cannot be used \
+                                                                         \with coins."
+        ([ Left sorryMsg    ], _                   ) -> wrapSend mq cols sorryMsg
+        ([ Right (_:_:_)    ], _                   ) -> wrapSend mq cols "Sorry, but you can only target one person at \
+                                                                         \a time with action commands."
+        ([ Right [targetId] ], _                   ) ->
+          let (view sing -> targetSing) = (ws^.entTbl) ! targetId
+              actionOnPC targetDesig    =
+                  let toSelfMsg      = "You blink at " <> targetDesig <> "."
+                      toSelfBrdcst   = (nlnl toSelfMsg, [i])
+                      toTargetMsg    = serialize d <> " blinks at you."
+                      toTargetBrdcst = (nlnl toTargetMsg, [targetId])
+                      toOthersMsg    = T.concat [ serialize d, " blinks at ", targetDesig, "." ]
+                      toOthersBrdcst = (nlnl toOthersMsg, pcIds d \\ [ i, targetId ])
+                  in do
+                      logPlaOut (bracketQuote "action command") i [ parsePCDesig i ws toSelfMsg ]
+                      bcast $ toSelfBrdcst : toTargetBrdcst : [toOthersBrdcst]
+              actionOnMob targetNoun    =
+                  let toSelfMsg      = "You blink at " <> targetNoun <> "."
+                      toSelfBrdcst   = (nlnl toSelfMsg, [i])
+                      toOthersMsg    = T.concat [ serialize d, " blinks at ", targetNoun, "." ]
+                      toOthersBrdcst = (nlnl toOthersMsg, i `delete` pcIds d)
+                  in do
+                      logPlaOut (bracketQuote "action command") i [toSelfMsg]
+                      bcast $ toSelfBrdcst : [toOthersBrdcst]
+          in case (ws^.typeTbl) ! targetId of
+            PCType  -> actionOnPC  . serialize . mkStdDesig targetId ws targetSing False $ ris
+            MobType -> actionOnMob . theOnLower $ targetSing
+            _       -> wrapSend mq cols "Sorry, but action commands may only target people."
+        x -> patternMatchFail "TODO" [ showText x ]
+      else wrapSend mq cols "You don't see anyone here."
 actionCmdBlink _ = return () -- TODO
