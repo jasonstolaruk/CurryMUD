0391869c33b05e7a34cd6cc4775be64d59fd1994
Jason Stolaruk <jason@detroitlabs.com>
2015-02-25 10:13:42 -0500

    親友が号泣。:six_pointed_star:

--- lib/Mud/Cmds/Debug.hs
+++ lib/Mud/Cmds/Debug.hs
@@ -452,7 +452,7 @@ debugWrap (WithArgs i mq cols [a]) = case reads . T.unpack $ a :: [(Int, String)
       | otherwise                              = do
           logPlaExecArgs (prefixDebugCmd "wrap") [a] i
           send mq . frame lineLen . wrapUnlines lineLen $ msg
-    sorryWtf     = wrapSend mq cols $ wtfColor <> "Hey! Are you trying to make me crash?" <> dfltColor
+    sorryWtf     = wrapSend mq cols $ wtfColor <> "He don't." <> dfltColor
     sorryLineLen = wrapSend mq cols . T.concat $ [ "The line length must be between "
                                                  , showText minCols
                                                  , " and "
@@ -508,7 +508,7 @@ debugWrapIndent (WithArgs i mq cols [a, b]) = do
       | otherwise                              = do
           logPlaExecArgs (prefixDebugCmd "wrapindent") [a, b] i
           send mq . frame lineLen . T.unlines . wrapIndent indent lineLen $ msg
-    sorryWtf     = wrapSend mq cols $ wtfColor <> "Hey! Are you trying to make me crash?" <> dfltColor
+    sorryWtf     = wrapSend mq cols $ wtfColor <> "He don't." <> dfltColor
     sorryLineLen = wrapSend mq cols . T.concat $ [ "The line length must be between "
                                                  , showText minCols
                                                  , " and "
