1c481c1d00207c4a87fc6699f9f3866806f8ef42
Jason Stolaruk <jason@detroitlabs.com>
2015-03-05 13:53:34 -0500

    傷害罪で収監された男。:shirt:

--- tests/MudTests/TheWorld/TheWorld.hs
+++ tests/MudTests/TheWorld/TheWorld.hs
@@ -3,7 +3,10 @@ module MudTests.TheWorld.TheWorld where
 import Mud.Data.State.State
 import MudTests.TestUtil
 
+import Control.Concurrent.STM.TVar (readTVarIO)
 import Control.Lens.Getter (view)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Reader (ask)
 import Data.List (group)
 import Test.QuickCheck.Monadic (assert, monadicIO)
 import Test.Tasty.QuickCheck (Property)
@@ -11,6 +14,7 @@ import qualified Data.IntMap.Lazy as IM (elems)
 
 
 prop_noDupIds :: Property
-prop_noDupIds = monadicIO $ getInvTbl >>= assert . not . any ((> 1) . length) . group . concat . IM.elems
+prop_noDupIds = monadicIO $ do
+    getInvTbl >>= assert . not . any ((> 1) . length) . group . concat . IM.elems
   where
-    getInvTbl = inWorld $ view invTbl `fmap` readWSTMVar
+    getInvTbl = inWorld $ liftIO . readTVarIO . view invTblTVar =<< ask
