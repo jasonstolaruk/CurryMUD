eb384703bbb3dfabfc0094e25588ec87d10b5994
Jason P. Stolaruk <jason@detroitlabs.com>
2015-11-29 12:29:09 -0500

    色鉛筆終了困る。:anguished:

--- lib/Mud/TopLvlDefs/Misc.hs
+++ lib/Mud/TopLvlDefs/Misc.hs
@@ -3,9 +3,10 @@
 module Mud.TopLvlDefs.Misc where
 
 import Mud.TopLvlDefs.Chars
+import Paths_currymud
 
 import Data.Monoid ((<>))
-import Data.Version (Version, makeVersion, showVersion)
+import Data.Version (showVersion)
 import System.Posix.Types (FileOffset)
 import qualified Data.Text as T
 
@@ -138,10 +139,6 @@ threadTblPurgerDelay = 60 * 60 -- one hr
 
 ver :: T.Text
 ver = T.pack . showVersion $ version
-
-
-version :: Version
-version = makeVersion [ 0, 1, 0, 0 ]
 
 
 worldPersisterDelay :: Int
