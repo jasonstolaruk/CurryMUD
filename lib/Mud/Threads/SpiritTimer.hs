181c67b91380d90687c8acd1828dc26f50c661de
Jason P. Stolaruk <jason@detroitlabs.com>
2016-10-30 15:25:47 -0400

    "threadSpiritTimer". :tiger2:

--- lib/Mud/Threads/SpiritTimer.hs
+++ lib/Mud/Threads/SpiritTimer.hs
@@ -1,8 +1,8 @@
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Mud.Threads.SpiritTimer ( runSpiritAsync
-                               , throwWaitSpirit ) where
+module Mud.Threads.SpiritTimer ( runSpiritTimerAsync
+                               , throwWaitSpiritTimer ) where
 
 -- import Mud.Data.Misc
 import Mud.Data.State.MudData
@@ -14,16 +14,16 @@ import Mud.Util.Misc
 import Mud.Util.Operators
 -- import Mud.Util.Quoting
 -- import Mud.Util.Text
--- import qualified Mud.Misc.Logging as L (logExMsg, logPla)
+import qualified Mud.Misc.Logging as L (logPla)
 
--- import Control.Concurrent (threadDelay)
+import Control.Concurrent (threadDelay)
 -- import Control.Exception (AsyncException(..), SomeException, fromException)
--- import Control.Exception.Lifted (catch, finally)
+import Control.Exception.Lifted (handle)
 import Control.Lens.Operators ((&), (.~), (?~), (^.))
-import Control.Monad ((>=>))
--- import Control.Monad.IO.Class (liftIO)
+import Control.Monad ((>=>), forever)
+import Control.Monad.IO.Class (liftIO)
 -- import Data.Monoid ((<>))
--- import Data.Text (Text)
+import Data.Text (Text)
 -- import qualified Data.Map.Lazy as M (delete, empty, insert, lookup, toList)
 -- import qualified Data.Text as T
 
@@ -37,22 +37,22 @@ default (Int)
 {-
 logExMsg :: Text -> Text -> SomeException -> MudStack ()
 logExMsg = L.logExMsg "Mud.Threads.SpiritTimer"
+-}
 
 
 logPla :: Text -> Id -> Text -> MudStack ()
 logPla = L.logPla "Mud.Threads.SpiritTimer"
--}
 
 
 -- ==================================================
 
 
-runSpiritAsync :: Id -> MudStack ()
-runSpiritAsync i = runAsync (threadSpirit i) >>= \a -> tweak $ plaTbl.ind i.spiritAsync ?~ a
+runSpiritTimerAsync :: Id -> MudStack ()
+runSpiritTimerAsync i = runAsync (threadSpiritTimer i) >>= \a -> tweak $ plaTbl.ind i.spiritAsync ?~ a
 
 
-throwWaitSpirit :: Id -> MudStack ()
-throwWaitSpirit i = helper |&| modifyState >=> maybeVoid throwWait
+throwWaitSpiritTimer :: Id -> MudStack ()
+throwWaitSpiritTimer i = helper |&| modifyState >=> maybeVoid throwWait
   where
     helper ms = let a = ms^.plaTbl.ind i.spiritAsync
                 in (ms & plaTbl.ind i.spiritAsync .~ Nothing, a)
@@ -61,5 +61,12 @@ throwWaitSpirit i = helper |&| modifyState >=> maybeVoid throwWait
 -----
 
 
-threadSpirit :: Id -> MudStack ()
-threadSpirit _ = unit
+threadSpiritTimer :: Id -> MudStack ()
+threadSpiritTimer i = handle (threadExHandler (Just i) "spirit timer") $ do
+    setThreadType . SpiritTimer $ i
+    let loop = (liftIO . threadDelay $ 1 * 10 ^ 6) >> spiritTimer i
+    handle (die (Just i) "spirit") $ logPla "threadSpiritTimer" i "digester started." >> forever loop
+
+
+spiritTimer :: Id -> MudStack ()
+spiritTimer _ = unit
