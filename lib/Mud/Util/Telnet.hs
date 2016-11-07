5d6b55ca715fc3e80ec838d3f9c6a475f3d6ede4
Jason P. Stolaruk <jason@detroitlabs.com>
2016-11-06 23:03:44 -0500

    Tweaking "parseTelnet". :page_with_curl:

--- lib/Mud/Util/Telnet.hs
+++ lib/Mud/Util/Telnet.hs
@@ -45,8 +45,12 @@ parseTelnet = f ("", [])
                   let telnets = [ TCode TelnetIAC, TCode TelnetSB ] ++ mkTelnetDatas others
                       others  = y : T.unpack codes ++ pure telnetSE
                   in f (msg <> left, td ++ telnets) rest'
-          | otherwise = -- Assume that both of the 2 bytes following IAC are part of the telnet command.
+          | x `elem` [ telnetWILL, telnetWON'T, telnetDO, telnetDON'T ] =
+              -- It's a 3-byte command (including the IAC).
               f (msg <> left, td ++ pure (TCode TelnetIAC) ++ mkTelnetDatas [ x, y ]) rest
+          | otherwise =
+              -- It's a 2-byte command (including the IAC).
+              f (msg <> left, td ++ pure (TCode TelnetIAC) ++ mkTelnetDatas (pure x)) $ y `T.cons` rest
         -- There is a single IAC and nothing else, or an IAC followed by just one byte:
         helper (left, right) | right == T.singleton telnetIAC = (msg <> left, td ++ pure (TCode TelnetIAC))
                              | T.length right == 2 = let telnets = TCode TelnetIAC : mkTelnetDatas others
