e6a903ab08e9b3933116f24e84a2292e093991eb
Jason P. Stolaruk <jason@detroitlabs.com>
2016-11-18 23:17:10 -0500

    Updates to Lua stuff. :straight_ruler:

--- lib/Mud/Data/State/Util/GMCP.hs
+++ lib/Mud/Data/State/Util/GMCP.hs
@@ -9,7 +9,7 @@ import Mud.TheWorld.Zones.ZoneMap
 import Mud.Util.Quoting
 import Mud.Util.Text
 
-import Control.Lens (both, each, to, views)
+import Control.Lens (both, each, views)
 import Control.Lens.Operators ((%~), (&), (^.))
 import Data.Monoid ((<>))
 import Data.Text (Text)
@@ -24,24 +24,24 @@ comma = ", "
 gmcpVitals :: Id -> MudState -> Text
 gmcpVitals i ms = "Char.Vitals " <> curlyQuote (spaced rest)
   where
-    rest = T.concat [ dblQuote "hp"     <> colon
-                    , hpCurr            <> comma
-                    , dblQuote "max_hp" <> colon
-                    , hpMax             <> comma
+    rest = T.concat [ dblQuote "curr_hp" <> colon
+                    , hpCurr             <> comma
+                    , dblQuote "max_hp"  <> colon
+                    , hpMax              <> comma
                     ----------
-                    , dblQuote "mp"     <> colon
-                    , mpCurr            <> comma
-                    , dblQuote "max_mp" <> colon
-                    , mpMax             <> comma
+                    , dblQuote "curr_mp" <> colon
+                    , mpCurr             <> comma
+                    , dblQuote "max_mp"  <> colon
+                    , mpMax              <> comma
                     ----------
-                    , dblQuote "pp"     <> colon
-                    , ppCurr            <> comma
-                    , dblQuote "max_pp" <> colon
-                    , ppMax             <> comma
+                    , dblQuote "curr_pp" <> colon
+                    , ppCurr             <> comma
+                    , dblQuote "max_pp"  <> colon
+                    , ppMax              <> comma
                     ----------
-                    , dblQuote "fp"     <> colon
-                    , fpCurr            <> comma
-                    , dblQuote "max_fp" <> colon
+                    , dblQuote "curr_fp" <> colon
+                    , fpCurr             <> comma
+                    , dblQuote "max_fp"  <> colon
                     , fpMax ]
     ((hpCurr, hpMax), (mpCurr, mpMax), (ppCurr, ppMax), (fpCurr, fpMax)) = f
     f = getPts i ms & each %~ (both %~ (dblQuote . showText))
@@ -59,18 +59,18 @@ gmcpRmInfo i ms = "Room.Info " <> curlyQuote (spaced rest)
                     , dblQuote "room_name"  <> colon
                     , dblQuote roomName     <> comma
                     , dblQuote "x_coord"    <> colon
-                    , dblQuote xCoord       <> comma
+                    , showText xCoord       <> comma
                     , dblQuote "y_coord"    <> colon
-                    , dblQuote yCoord       <> comma
+                    , showText yCoord       <> comma
                     , dblQuote "z_coord"    <> colon
-                    , dblQuote zCoord       <> comma
+                    , showText zCoord       <> comma
                     , dblQuote "room_exits" <> colon
                     , exits ]
     ri                       = getRmId i ms
     (zoneId, zoneName)       = getZoneForRmId ri
     rm                       = getRm ri ms
     roomName                 = rm^.rmName
-    (xCoord, yCoord, zCoord) = rm^.rmCoords.to (each %~ showText)
+    (xCoord, yCoord, zCoord) = rm^.rmCoords
     exits                    = curlyQuote . spaced . views rmLinks (commas . map mkExitTxt) $ rm
       where
         mkExitTxt (StdLink    dir destId _    ) = f (linkDirToCmdName dir) destId
