529c02ae39fb200847ad8415a47649202912751b
Jason P. Stolaruk <jason@detroitlabs.com>
2016-12-30 03:53:25 -0500

    "CurryTime". :cat:

--- lib/Mud/Data/State/Util/CurryTime.hs
+++ lib/Mud/Data/State/Util/CurryTime.hs
@@ -1,43 +1,164 @@
-{-# OPTIONS_GHC -fno-warn-type-defaults -Wno-redundant-constraints #-} -- TODO: Needed?
-{-# LANGUAGE LambdaCase, OverloadedStrings, TupleSections, ViewPatterns #-} -- TODO: Needed?
+{-# OPTIONS_GHC -Wno-redundant-constraints #-} -- TODO: Needed?
+{-# LANGUAGE OverloadedStrings #-}
 
 module Mud.Data.State.Util.CurryTime where
 
-import Mud.Data.Misc
-import Mud.Data.State.MudData
-import Mud.Data.State.Util.Calc
-import Mud.Data.State.Util.Get
-import Mud.Data.State.Util.Misc
-import Mud.Data.State.Util.Output
-import Mud.Misc.Misc
-import Mud.TopLvlDefs.Misc
-import Mud.Util.List
-import Mud.Util.Misc
-import Mud.Util.Operators
-import Mud.Util.Quoting
-import Mud.Util.Text
-import qualified Mud.Misc.Logging as L (logNotice)
+-- import Mud.Data.Misc
+-- import Mud.Data.State.MudData
+-- import Mud.Data.State.Util.Misc
+-- import Mud.Misc.Misc
+-- import Mud.TopLvlDefs.Misc
+-- import Mud.Util.Misc
+-- import Mud.Util.Operators
+-- import Mud.Util.Quoting
 
-import Control.Arrow ((***), first)
-import Control.Lens (_1, _2, _3, at, view, views)
-import Control.Lens.Operators ((%~), (&), (.~), (<>~), (^.))
-import Control.Monad (forM_, unless, when)
-import Control.Monad.IO.Class (liftIO)
-import Data.List (delete, sortBy)
-import Data.Monoid ((<>))
-import Data.Text (Text)
-import GHC.Stack (HasCallStack)
-import qualified Data.Text as T
+-- import Data.Monoid ((<>))
+-- import Data.Text (Text)
+import Data.Time (UTCTime(..), diffUTCTime, fromGregorian, getCurrentTime)
+-- import GHC.Stack (HasCallStack)
+import Prelude hiding (min)
+-- import qualified Data.Text as T
 
 
-default (Int)
+type Year  = Integer
+type Month = Integer
+type Week  = Integer
+type Day   = Integer
+type Hour  = Integer
+type Min   = Integer
+type Sec   = Integer
 
 
------
+-- Months
+curryMonthsInYear :: Month
+curryMonthsInYear = 8
 
 
-logNotice :: Text -> Text -> MudStack ()
-logNotice = L.logNotice "Mud.Data.State.Util.CurryTime"
+-- Weeks
+curryWeeksInMonth :: Week
+curryWeeksInMonth = 3
+
+
+curryWeeksInYear :: Week
+curryWeeksInYear = curryWeeksInMonth * curryMonthsInYear
+
+
+-- Days
+curryDaysInWeek :: Day
+curryDaysInWeek = 7
+
+
+curryDaysInMonth :: Day
+curryDaysInMonth = curryDaysInWeek * curryWeeksInMonth
+
+
+curryDaysInYear :: Day
+curryDaysInYear = curryDaysInMonth * curryMonthsInYear
+
+
+-- Hours
+curryHoursInDay :: Hour
+curryHoursInDay = 20
+
+
+curryHoursInWeek :: Hour
+curryHoursInWeek = curryHoursInDay * curryDaysInWeek
+
+
+curryHoursInMonth :: Hour
+curryHoursInMonth = curryHoursInWeek * curryWeeksInMonth
+
+
+curryHoursInYear :: Hour
+curryHoursInYear = curryHoursInMonth * curryMonthsInYear
+
+
+-- Mins
+curryMinsInHour :: Min
+curryMinsInHour = 60
+
+
+curryMinsInDay :: Min
+curryMinsInDay = curryMinsInHour * curryHoursInDay
+
+
+curryMinsInWeek :: Min
+curryMinsInWeek = curryMinsInDay * curryDaysInWeek
+
+
+curryMinsInMonth :: Min
+curryMinsInMonth = curryMinsInWeek * curryWeeksInMonth
+
+
+curryMinsInYear :: Min
+curryMinsInYear = curryMinsInMonth * curryMonthsInYear
+
+
+-- Seconds
+currySecsInMin :: Sec
+currySecsInMin = 60
+
+
+currySecsInHour :: Sec
+currySecsInHour = currySecsInMin * curryMinsInHour
+
+
+currySecsInDay :: Sec
+currySecsInDay = currySecsInHour * curryHoursInDay
+
+
+currySecsInWeek :: Sec
+currySecsInWeek = currySecsInDay * curryDaysInWeek
+
+
+currySecsInMonth :: Sec
+currySecsInMonth = currySecsInWeek * curryWeeksInMonth
+
+
+currySecsInYear :: Sec
+currySecsInYear = currySecsInMonth * curryMonthsInYear
 
 
 -- ==================================================
+
+
+data CurryTime = CurryTime { curryYear       :: Year
+                           , curryMonth      :: Month
+                           , curryWeek       :: Week
+                           , curryDayOfMonth :: Day
+                           , curryDayOfWeek  :: Day
+                           , curryHour       :: Hour
+                           , curryMin        :: Min
+                           , currySec        :: Sec }
+
+
+curryEpoch :: UTCTime
+curryEpoch = UTCTime (fromGregorian 2016 1 1) 0
+
+
+getSecsFromCurryEpoch :: IO Sec
+getSecsFromCurryEpoch = round . (`diffUTCTime` curryEpoch) <$> getCurrentTime
+
+
+getCurryTime :: IO CurryTime
+getCurryTime = secsToCurryTime <$> getSecsFromCurryEpoch
+
+
+secsToCurryTime :: Sec -> CurryTime
+secsToCurryTime x = let years  = x `div` currySecsInYear
+                        months = x `div` currySecsInMonth
+                        weeks  = x `div` currySecsInWeek
+                        days   = x `div` currySecsInDay
+                        hours  = x `div` currySecsInHour
+                        mins   = x `div` currySecsInMin
+                        secs   = x
+                        -----
+                        year       = succ $ years + 200
+                        month      = succ $ months `rem` curryMonthsInYear
+                        week       = succ $ weeks  `rem` curryWeeksInMonth
+                        dayOfMonth = succ $ days   `rem` curryDaysInMonth
+                        dayOfWeek  = succ $ days   `rem` curryDaysInWeek
+                        hour       = succ $ hours  `rem` curryHoursInDay
+                        min        = succ $ mins   `rem` curryMinsInHour
+                        sec        = succ $ secs   `rem` currySecsInMin
+                    in CurryTime year month week dayOfMonth dayOfWeek hour min sec
