6e203ca3a670449ccf24d5b461278cdd3d8bb99e
Jason P. Stolaruk <jason@detroitlabs.com>
2017-01-26 17:16:37 -0500

    Destroying holy symbols. :syringe:

--- lib/Mud/Data/State/Util/Destroy.hs
+++ lib/Mud/Data/State/Util/Destroy.hs
@@ -26,44 +26,46 @@ destroyHelper :: Inv -> MudState -> MudState
 destroyHelper = flip . foldr $ helper
   where
     helper i ms = case getType i ms of
-      ArmType      -> upd ms [ destroyEnt, destroyObj, destroyArm,   rest ]
-      ClothType    -> upd ms [ destroyEnt, destroyObj, destroyCloth, rest ]
-      ConType      -> upd ms [ destroyCont
-                             , destroyEnt
-                             , destroyObj
-                             , destroyInv
-                             , destroyCoins
-                             , destroyCloth
-                             , destroyCon
-                             , rest ]
-      CorpseType   -> upd ms [ destroyCont
-                             , destroyEnt
-                             , destroyObj
-                             , destroyInv
-                             , destroyCoins
-                             , destroyCloth
-                             , destroyCon
-                             , destroyCorpse
-                             , rest ]
-      FoodType     -> upd ms [ destroyEnt, destroyObj, destoryFood,     rest ]
-      ObjType      -> upd ms [ destroyEnt, destroyObj,                  rest ]
-      VesselType   -> upd ms [ destroyEnt, destroyObj, destroyVessel,   rest ]
-      WpnType      -> upd ms [ destroyEnt, destroyObj, destroyWpn,      rest ]
-      WritableType -> upd ms [ destroyEnt, destroyObj, destroyWritable, rest ]
-      _            -> ms
+      ArmType        -> upd ms [ destroyEnt, destroyObj, destroyArm,   rest ]
+      ClothType      -> upd ms [ destroyEnt, destroyObj, destroyCloth, rest ]
+      ConType        -> upd ms [ destroyCont
+                               , destroyEnt
+                               , destroyObj
+                               , destroyInv
+                               , destroyCoins
+                               , destroyCloth
+                               , destroyCon
+                               , rest ]
+      CorpseType     -> upd ms [ destroyCont
+                               , destroyEnt
+                               , destroyObj
+                               , destroyInv
+                               , destroyCoins
+                               , destroyCloth
+                               , destroyCon
+                               , destroyCorpse
+                               , rest ]
+      FoodType       -> upd ms [ destroyEnt, destroyObj, destoryFood,       rest ]
+      HolySymbolType -> upd ms [ destroyEnt, destroyObj, destroyHolySymbol, rest ]
+      ObjType        -> upd ms [ destroyEnt, destroyObj,                    rest ]
+      VesselType     -> upd ms [ destroyEnt, destroyObj, destroyVessel,     rest ]
+      WpnType        -> upd ms [ destroyEnt, destroyObj, destroyWpn,        rest ]
+      WritableType   -> upd ms [ destroyEnt, destroyObj, destroyWritable,   rest ]
+      _              -> ms
       where
-        destroyArm      = armTbl     .at i .~ Nothing
-        destroyCloth    = clothTbl   .at i .~ Nothing
-        destroyCoins    = coinsTbl   .at i .~ Nothing
-        destroyCon      = conTbl     .at i .~ Nothing
-        destroyCorpse   = corpseTbl  .at i .~ Nothing
-        destroyEnt      = flip upd [ entTbl.at i .~ Nothing, pausedEffectsTbl.at i .~ Nothing ]
-        destoryFood     = foodTbl    .at i .~ Nothing
-        destroyInv      = invTbl     .at i .~ Nothing
-        destroyObj      = objTbl     .at i .~ Nothing
-        destroyType     = typeTbl    .at i .~ Nothing
-        destroyVessel   = vesselTbl  .at i .~ Nothing
-        destroyWpn      = wpnTbl     .at i .~ Nothing
-        destroyWritable = writableTbl.at i .~ Nothing
-        destroyCont ms' = foldr helper ms' . getInv i $ ms'
-        rest            = flip upd [ destroyType, invTbl %~ IM.map (i `delete`) ]
+        destroyArm        = armTbl       .at i .~ Nothing
+        destroyCloth      = clothTbl     .at i .~ Nothing
+        destroyCoins      = coinsTbl     .at i .~ Nothing
+        destroyCon        = conTbl       .at i .~ Nothing
+        destroyCorpse     = corpseTbl    .at i .~ Nothing
+        destroyEnt        = flip upd [ entTbl.at i .~ Nothing, pausedEffectsTbl.at i .~ Nothing ]
+        destoryFood       = foodTbl      .at i .~ Nothing
+        destroyHolySymbol = holySymbolTbl.at i .~ Nothing
+        destroyInv        = invTbl       .at i .~ Nothing
+        destroyObj        = objTbl       .at i .~ Nothing
+        destroyType       = typeTbl      .at i .~ Nothing
+        destroyVessel     = vesselTbl    .at i .~ Nothing
+        destroyWpn        = wpnTbl       .at i .~ Nothing
+        destroyWritable   = writableTbl  .at i .~ Nothing
+        destroyCont ms'   = foldr helper ms' . getInv i $ ms'
+        rest              = flip upd [ destroyType, invTbl %~ IM.map (i `delete`) ]
