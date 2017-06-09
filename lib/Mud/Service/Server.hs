c5b92951e157d63666fe7a10bf7a1b23c01e85f6
Jason Stolaruk <jason@detroitlabs.com>
2017-06-09 00:14:08 -0400

    No more "screen". :school:

--- lib/Mud/Service/Server.hs
+++ lib/Mud/Service/Server.hs
@@ -11,8 +11,6 @@ import           Control.Monad.Error.Class (throwError)
 import           Control.Monad.IO.Class (liftIO)
 import           Data.IORef (IORef, readIORef)
 import qualified Data.IntMap.Strict as IM (IntMap, elems, mapWithKey)
-import           Data.Text (Text)
-import qualified Data.Text.IO as T
 import           GHC.Stack (HasCallStack)
 import           Servant (Handler, Header, Headers, NoContent(..), Server, (:<|>)(..), err401, err404, errBody, serveDirectoryFileServer)
 import           Servant.Auth.Server (AuthResult(..), CookieSettings, JWTSettings, SetCookie, acceptLogin, throwAll)
@@ -25,11 +23,6 @@ server cs jwts = (:<|>) <$> protected <*> unprotected cs jwts
 -----
 
 
-{-
-curl -H "Content-Type: application/json" \
-     -H "Authorization: Bearer tokenHere" \
-     localhost:7249/db/alertexecrec/all -v
--}
 protected :: HasCallStack => IORef MudState -> AuthResult Login -> Server Protected
 protected ior (Authenticated _) =
          getAllPla
@@ -40,15 +33,36 @@ protected ior (Authenticated _) =
     state :: HasCallStack => Handler MudState
     state = liftIO . readIORef $ ior
 
+{-
+curl -H "Content-Type: application/json" \
+     -H "Authorization: Bearer tokenHere" \
+     localhost:7249/pla/all -v
+-}
     getAllPla :: HasCallStack => Handler [Object Pla]
     getAllPla = views plaTbl mkObjects <$> state
 
+{-
+curl -H "Content-Type: application/json" \
+     -H "Authorization: Bearer tokenHere" \
+     localhost:7249/pla/0 -v
+-}
     getPlaById :: HasCallStack => CaptureInt -> Handler (Object Pla)
     getPlaById (CaptureInt i) = views (plaTbl.at i) (maybe notFound (return . Object i)) =<< state
 
+{-
+curl -H "Content-Type: application/json" \
+     -H "Authorization: Bearer tokenHere" \
+     localhost:7249/db/alertexecrec/all -v
+-}
     getAllAlertExecRec :: HasCallStack => Handler [AlertExecRec]
     getAllAlertExecRec = liftIO . getDbTblRecs $ "alert_exec"
 
+{-
+curl -X DELETE \
+     -H "Content-Type: application/json" \
+     -H "Authorization: Bearer tokenHere" \
+     localhost:7249/db/alertexecrec/1 -v
+-}
     deleteAlertExecRec :: HasCallStack => CaptureInt -> Handler NoContent
     deleteAlertExecRec (CaptureInt i) = liftIO $ deleteDbTblRec "alert_exec" i >> return NoContent
 protected _ _ = throwAll err401 -- Unauthorized
@@ -73,10 +87,6 @@ unprotected cs jwts _ =
     :<|> serveDirectoryFileServer "notes"
 
 
-screen :: HasCallStack => Text -> Handler ()
-screen = liftIO . T.putStrLn
-
-
 loginHelper :: HasCallStack => CookieSettings
                             -> JWTSettings
                             -> Login
@@ -85,5 +95,5 @@ loginHelper :: HasCallStack => CookieSettings
 loginHelper cs jwts login@(Login un pw)
   | un == "curry", pw == "curry" = liftIO (acceptLogin cs jwts login) >>= \case
     Nothing           -> throwError err401
-    Just applyCookies -> screen "Authenticated." >> return (applyCookies NoContent)
-  | otherwise = screen "Bad creds." >> throwError err401
+    Just applyCookies -> return (applyCookies NoContent)
+  | otherwise = throwError err401
