63c3bdaab582b1d5ee6f0cd57611a7c6aeadb826
Jason Stolaruk <jason@detroitlabs.com>
2017-11-16 01:56:30 -0500

    Sure. :sushi:

--- lib/Mud/Data/State/Util/Output.hs
+++ lib/Mud/Data/State/Util/Output.hs
@@ -93,7 +93,7 @@ anglePrompt = flip sendPrompt ">"
 
 
 -- Because "bcast" calls "parseInBands" with "CurryTime", it should not be used to send a "toSelf" message to the
--- executor of a cmd in the case that said "toSelf" message contains a serialized "Desig" or "VerbObj".
+-- executor of a cmd in the case that said message contains a serialized "Desig" or "VerbObj".
 -- TODO: Review uses of "bcast".
 bcast :: HasCallStack => [Broadcast] -> MudStack ()
 bcast [] = unit
